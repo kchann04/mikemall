e1e7fad8b0cbfc0cdae3dbff6b4905f924ea5983
kchann04 <skc1979@naver.com>
2021-12-06 15:15:51 +0900

    최초업로드
    최초업로드

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){//시작
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  }); // swiper 끝
+
+  $(".tab_title li").click(function(){
+    event.preventDefault();
+    var idx = $(this).index();
+
+   $(".tab_title li").removeClass("on");
+   $(".tab_title li").eq(idx).addClass("on");
+   $(".tab_con > .cont").hide();
+   $(".tab_con > .cont").eq(idx).show();
+  });// tab 끝
+
+  $(".btn_box .menu").click(function(){
+    $("header .gnb").animate({"right":"0"},500);
+  });
+
+});//끝
