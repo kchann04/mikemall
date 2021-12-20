42f0efca91586f0dad91f6a7dccea4ea5d29b632
ffsdfsfdsdf <skc1979@naver.com>
2021-12-20 14:55:18 +0900

    수정
    수정

--- js/main.js
+++ js/main.js
@@ -24,16 +24,56 @@ $(document).ready(function(){//시작
    $(".tab_con > .cont").eq(idx).show();
   });// tab 끝
 
-  $(".btn_box .menu").click(function(){
-    $(this).hide(500);
-    $(".btn_box .close").show(500);
-    $("header .gnb").animate({"right":"0"},500);
-  });
+  
+  startWidth();
+  resonsiveWidth();
+  $(window).resize(resonsiveWidth);
 
-  $(".btn_box .close").click(function(){
-    $(this).hide(500);
+  function startWidth(){
+    win = $(window).width();
+    if (win < 1200) {
+      mobileBox();
+    } else {
+      pcBox();
+    }
+  }
+
+  function resonsiveWidth() {
+  
+    var windowsize = $(window).width();
+
+    if (windowsize < 1201) {
+      mobileBox();
+    }else{
+      pcBox();
+    }
+  }
+
+
+  function mobileBox() {
+    // 모바일에서 실행 내용
+    $("header .gnb").css({"right":"-200px"});
     $(".btn_box .menu").show(500);
-    $("header .gnb").animate({"right":"-200px"},500)
-  });
+
+    $(".btn_box .menu").click(function(){
+      $(this).hide(500);
+      $(".btn_box .close").show(500);
+      $("header .gnb").animate({"right":"0"},500);
+    });
+  
+    $(".btn_box .close").click(function(){
+      $(this).hide(500);
+      $(".btn_box .menu").show(500);
+      $("header .gnb").animate({"right":"-200px"},500)
+    });
+  }
+
+  function pcBox() {
+    $(".btn_box .menu").hide();
+    $(".btn_box .close").hide();
+    $("header .gnb").css({"right":"170px"});
+   
+  }
+
 
 });//끝
